e987f2dbdb9ba3ec7885a08981f035e8f1a22d5e
soycheeto <trishahansen.ihs@gmail.com>
2024-05-07 10:35:37 +0530

    modified main()

--- three/main.c
+++ three/main.c
@@ -42,14 +42,36 @@ int playMove(char board[SIZE][SIZE], char player, int *x, int *y){
 
 }
 
-scoreBoard(char board[SIZE][SIZE], int countX[SIZE], int countO[SIZE]);
+int scoreBoard(char board[SIZE][SIZE], int countX[SIZE], int countO[SIZE]){
+    /*
+    for i in SIZE;
+    if board[]
+    */
+}
 printScores(char board[SIZE][SIZE]);
 playGame(char board[SIZE][SIZE], char player1,char player2);
 
 int main(void){
     char board[SIZE][SIZE];
     initializeBoard(board);
+    printBoard(board);
     char player1 = 'x';
     char player2 = 'o';
+    char p = player1;
+    int x,y;
+    int countX[SIZE], countO[SIZE];
+    int win = scoreBoard(board, countX, countO);
+    playMove(board, p, &x, &y);
+    printBoard(board);
+    while(!scoreBoard(board, countX, countO)){
+        playMove(board, p, countX, countO);
+                if(p ==player1){
+                    p=player2;
+                }
+                else{
+                    p=player1;
+                }
+        
+    }
     return 0;
 }
