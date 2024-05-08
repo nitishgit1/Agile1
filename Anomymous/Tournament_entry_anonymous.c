eb87836176791f6096bcee597f50819de92ef03c
Adi5424k <141307668+Adi5424k@users.noreply.github.com>
2024-05-08 19:49:47 +0530

    Update Tournament_entry_anonymous.c

--- Anomymous/Tournament_entry_anonymous.c
+++ Anomymous/Tournament_entry_anonymous.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
+#define SIZE 7
+
+void playMove(char board[SIZE][SIZE], char player, int *x, int *y) {
+    int centerX = SIZE / 2;
+    int centerY = SIZE / 2;
+    int offsetX = rand() % 3 - 1; 
+    int offsetY = rand() % 3 - 1; 
+    *x = centerX + offsetX;
+    *y = centerY + offsetY;
+    if (*x < 0 || *x >= SIZE) {
+        *x = rand() % SIZE; 
+    }
+    if (*y < 0 || *y >= SIZE) {
+        *y = rand() % SIZE; 
+    }
+    while (!isValidMove(board, *x, *y)) {
+        *x = rand() % SIZE;
+        *y = rand() % SIZE;
+    }
+}
